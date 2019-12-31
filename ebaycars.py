a78613835cdbcb80f6b2e3863df8426025fdd524
Minouneshan <42576650+Minouneshan@users.noreply.github.com>
2019-12-31 00:57:41 -0600

    Update ebaycars.py

--- ebaycars.py
+++ ebaycars.py
@@ -1,2 +1,6 @@
 #First of all, We import the Numpy & Pandas libraries. Then, You maybe noticed that we used encoding Argument with 'pd.read_csv'; because it is decoded by "Latin-1".
 #After that, We used DataFrame.info() and DataFrame.head() to undrastand more about our dataframe.
+import pandas as pd
+import numpy as np
+auto=pd.read_csv('autos.csv',encoding='Latin-1')
+auto
